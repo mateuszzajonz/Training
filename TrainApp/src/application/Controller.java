fbb565fc719d15f2be4c1b9cc6f30e9f61a30b60
Maciej Werszler <115154@student.uni.opole.pl>
2021-06-01 17:09:57 +0200

    - Added test training LIST of exercises

--- TrainApp/src/application/Controller.java
+++ TrainApp/src/application/Controller.java
@@ -3,12 +3,12 @@ package application;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.image.ImageView;
-import javafx.stage.Stage;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
-
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
 
 public class Controller {
 	@FXML
@@ -20,12 +20,32 @@ public class Controller {
     public ListView Main_ListView;
     public Button Side_Menu_btn;
     public GridPane Side_Menu_App;
+    public Button btn_CT;
+    public CheckBox chbox_muscle_CT;
+    public CheckBox chbox_endurance_CT;
+    public CheckBox chbox_strength_CT;
+    public TextField txtbox_minute_CT;
+    public TextField txtbox_hour_CT;
+    public CheckBox chbox_abs_CT;
+    public CheckBox chbox_arms_CT;
+    public CheckBox chbox_shoulders_CT;
+    public CheckBox chbox_chest_CT;
+    public CheckBox chbox_back_CT;
+    public CheckBox chbox_legs_CT;
     public ComboBox comboBox;
 
 	public void start() {
 
 
     }
-
 	
+    public void Create_Training()
+    {
+    	
+    }
+    
+    public void Open_Side_Menu()
+    {
+    	
+    }	
 }
--- TrainApp/src/application/Training.java
+++ TrainApp/src/application/Training.java
@@ -1,5 +1,25 @@
 package application;
 
-public class Training {
+import java.util.LinkedList;
+import java.util.List;
 
+public class Training
+{
+	List<String> listOfExercises = new LinkedList<>();
+
+	public void AddExercise(){
+	    listOfExercises.add("LEGS,BACK SQUAT");
+	    listOfExercises.add("LEGS,BULGARIAN SQUAT");
+	    listOfExercises.add("LEGS,DEADLIFT");
+	    listOfExercises.add("BACK,PENDLAY ROW");
+	    listOfExercises.add("BACK,DEADLIFT");
+	    listOfExercises.add("CHEST,BENCH PRESS");
+	    listOfExercises.add("CHEST,BUTTERFLY");
+	    listOfExercises.add("SHOULDERS,OVER HEAD PRESS");
+	    listOfExercises.add("SHOULDERS,LU RISES");
+	    listOfExercises.add("ARMS,PREACHER CURL");
+	    listOfExercises.add("ARMS,SKULL CRUSHERS");
+	    listOfExercises.add("ABS,PLANK");
+	    listOfExercises.add("ABS,CRUNCHES");
+	}
 }
