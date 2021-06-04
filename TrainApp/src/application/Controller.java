e866ef96c010153936a30f45e7d977fe931dd9b5
Maciej Werszler <115154@student.uni.opole.pl>
2021-06-04 12:05:41 +0200

    Fixed positioning of variables

--- TrainApp/src/application/Controller.java
+++ TrainApp/src/application/Controller.java
@@ -29,198 +29,193 @@ import javafx.scene.control.CheckBox;
 public class Controller {
 	@FXML
 
-    public AnchorPane Settings_App;
-    public AnchorPane Training_App;
-    public AnchorPane Main_App;
-    public ImageView Main_Image;
-    public ListView Main_ListView;
-    public ImageView img;
-    public Button Side_Menu_btn;
-    public GridPane Side_Menu_App;
-    
-    public ComboBox plec;
-    public TextField imie;
-    public TextField nazwisko;
-    public TextField waga;
-    public TextField wzrost;
-    public DatePicker data;
-    public Button Save_Click;
-    public Button Edit_Click;
-    public ScrollPane scrollPane;
+	public AnchorPane Settings_App;
+	public AnchorPane Training_App;
+	public AnchorPane Main_App;
+	public ImageView Main_Image;
+	public ListView Main_ListView;
+	public ImageView img;
+	public Button Side_Menu_btn;
+	public GridPane Side_Menu_App;
 
-    public Button btn_CT;
-    public TextField txtbox_minute_CT;
-    public TextField txtbox_hour_CT;
-    public CheckBox chbox_abs_CT;
-    public CheckBox chbox_arms_CT;
-    public CheckBox chbox_shoulders_CT;
-    public CheckBox chbox_chest_CT;
-    public CheckBox chbox_back_CT;
-    public CheckBox chbox_legs_CT;
-    public RadioButton radioBtn_muscle_CT;
-    public RadioButton radioBtn_endurance_CT;
-    public RadioButton radioBtn_strength_CT;
-    
+	public ComboBox plec;
+	public TextField imie;
+	public TextField nazwisko;
+	public TextField waga;
+	public TextField wzrost;
+	public DatePicker data;
+	public Button Save_Click;
+	public Button Edit_Click;
+	public ScrollPane scrollPane;
+
+	public Button btn_CT;
+	public TextField txtbox_minute_CT;
+	public TextField txtbox_hour_CT;
+	public CheckBox chbox_abs_CT;
+	public CheckBox chbox_arms_CT;
+	public CheckBox chbox_shoulders_CT;
+	public CheckBox chbox_chest_CT;
+	public CheckBox chbox_back_CT;
+	public CheckBox chbox_legs_CT;
+	public RadioButton radioBtn_muscle_CT;
+	public RadioButton radioBtn_endurance_CT;
+	public RadioButton radioBtn_strength_CT;
+
 	public void start(ActionEvent event) {
-		
-    }
 
-	public void SaveClick(ActionEvent event){
-			Profil p1 = new Profil(imie.getText(),nazwisko.getText(),waga.getText(),wzrost.getText(),data.getValue(),plec.getValue().toString(),1);
-            
-			try {
+	}
+
+	public void SaveClick(ActionEvent event) {
+		Profil p1 = new Profil(imie.getText(), nazwisko.getText(), waga.getText(), wzrost.getText(), data.getValue(),
+				plec.getValue().toString(), 1);
+
+		try {
 			FileOutputStream f = new FileOutputStream(new File("Profil.txt"));
-            ObjectOutputStream o = new ObjectOutputStream(f);
+			ObjectOutputStream o = new ObjectOutputStream(f);
 
-            // Write objects to file
-            o.writeObject(p1);
-            
-            o.close();
-            f.close();
+			// Write objects to file
+			o.writeObject(p1);
+
+			o.close();
+			f.close();
 
 			imie.setEditable(false);
-            nazwisko.setEditable(false);
-            wzrost.setEditable(false);
-            waga.setEditable(false);
-            data.setEditable(false);
-            plec.setEditable(false);
-            
-            imie.setDisable(true);
-            nazwisko.setDisable(true);
-            wzrost.setDisable(true);
-            waga.setDisable(true);
-            data.setDisable(true);
-            plec.setDisable(true);
-			
-            Edit_Click.setVisible(true);
-            Save_Click.setVisible(false);
-            
-			} catch (FileNotFoundException e) {
-	            System.out.println("File not found");
-	        } catch (IOException e) {
-	            System.out.println("Error initializing stream");
-	        } 
-           
-			
-    }
-	public void loadOnStart() {
-			loadProfil();
-			
-			plec.getItems().addAll("Mê¿czyzna","Kobieta");
+			nazwisko.setEditable(false);
+			wzrost.setEditable(false);
+			waga.setEditable(false);
+			data.setEditable(false);
+			plec.setEditable(false);
 
-            imie.setEditable(false);
-            nazwisko.setEditable(false);
-            wzrost.setEditable(false);
-            waga.setEditable(false);
-            data.setEditable(false);
-            plec.setEditable(false);
-            
-            imie.setDisable(true);
-            nazwisko.setDisable(true);
-            wzrost.setDisable(true);
-            waga.setDisable(true);
-            data.setDisable(true);
-            plec.setDisable(true);
-            
-            Save_Click.setVisible(false);
-            
-			ToggleGroup radioTrainGroup = new ToggleGroup();
-			radioBtn_strength_CT.setToggleGroup(radioTrainGroup);
-			radioBtn_endurance_CT.setToggleGroup(radioTrainGroup);
-			radioBtn_muscle_CT.setToggleGroup(radioTrainGroup);
+			imie.setDisable(true);
+			nazwisko.setDisable(true);
+			wzrost.setDisable(true);
+			waga.setDisable(true);
+			data.setDisable(true);
+			plec.setDisable(true);
+
+			Edit_Click.setVisible(true);
+			Save_Click.setVisible(false);
+
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found");
+		} catch (IOException e) {
+			System.out.println("Error initializing stream");
+		}
+
 	}
+
+	public void loadOnStart() {
+		loadProfil();
+
+		plec.getItems().addAll("Mê¿czyzna", "Kobieta");
+
+		imie.setEditable(false);
+		nazwisko.setEditable(false);
+		wzrost.setEditable(false);
+		waga.setEditable(false);
+		data.setEditable(false);
+		plec.setEditable(false);
+
+		imie.setDisable(true);
+		nazwisko.setDisable(true);
+		wzrost.setDisable(true);
+		waga.setDisable(true);
+		data.setDisable(true);
+		plec.setDisable(true);
+
+		Save_Click.setVisible(false);
+
+		ToggleGroup radioTrainGroup = new ToggleGroup();
+		radioBtn_strength_CT.setToggleGroup(radioTrainGroup);
+		radioBtn_endurance_CT.setToggleGroup(radioTrainGroup);
+		radioBtn_muscle_CT.setToggleGroup(radioTrainGroup);
+	}
+
 	private void loadProfil() {
 		try {
-		FileInputStream fi = new FileInputStream(new File("Profil.txt"));
-        ObjectInputStream oi = new ObjectInputStream(fi);
-		Profil pr1 = (Profil) oi.readObject();
-		
-        String[] dane = pr1.toString().split("\n");
-        
-        imie.setText(dane[0]);
-        nazwisko.setText(dane[1]);
-        waga.setText(dane[2]);
-        wzrost.setText(dane[3]);
-        data.setValue(LocalDate.parse(dane[4]));
-        plec.setValue(dane[5]);
-        
-       oi.close();
-       fi.close();
-		}		
-		catch (FileNotFoundException e) {
-           System.out.println("File not found");
-       }
-		catch (IOException e) {
-           System.out.println("Error initializing stream");
-       }
-		catch (ClassNotFoundException e) {
-           // TODO Auto-generated catch block
-           e.printStackTrace();
-       }
+			FileInputStream fi = new FileInputStream(new File("Profil.txt"));
+			ObjectInputStream oi = new ObjectInputStream(fi);
+			Profil pr1 = (Profil) oi.readObject();
+
+			String[] dane = pr1.toString().split("\n");
+
+			imie.setText(dane[0]);
+			nazwisko.setText(dane[1]);
+			waga.setText(dane[2]);
+			wzrost.setText(dane[3]);
+			data.setValue(LocalDate.parse(dane[4]));
+			plec.setValue(dane[5]);
+
+			oi.close();
+			fi.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found");
+		} catch (IOException e) {
+			System.out.println("Error initializing stream");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+
 	public void EditClick(ActionEvent event) {
-		
+
 		imie.setEditable(true);
-        nazwisko.setEditable(true);
-        wzrost.setEditable(true);
-        waga.setEditable(true);
-        data.setEditable(true);
-        plec.setEditable(true);
-        
-        imie.setDisable(false);
-        nazwisko.setDisable(false);
-        wzrost.setDisable(false);
-        waga.setDisable(false);
-        data.setDisable(false);
-        plec.setDisable(false);
-        
-        Edit_Click.setVisible(false);
-        Save_Click.setVisible(true);
+		nazwisko.setEditable(true);
+		wzrost.setEditable(true);
+		waga.setEditable(true);
+		data.setEditable(true);
+		plec.setEditable(true);
+
+		imie.setDisable(false);
+		nazwisko.setDisable(false);
+		wzrost.setDisable(false);
+		waga.setDisable(false);
+		data.setDisable(false);
+		plec.setDisable(false);
+
+		Edit_Click.setVisible(false);
+		Save_Click.setVisible(true);
 	}
+
 	public void ChooseImage(MouseEvent event) {
 		scrollPane.setVisible(true);
 	}
+
 	public void ChooseImageExit(MouseEvent event) {
 		scrollPane.setVisible(false);
 	}
-	
-    public void Create_Training()
-    {   	
-        Training_App.setVisible(true);
-        Settings_App.setVisible(false);
-        Main_App.setVisible(false);
-        Training trainCreate = new Training();
 
-        try
-        {
-        	int time = Integer.parseInt(txtbox_hour_CT.getText())+ Integer.parseInt(txtbox_minute_CT.getText());
-            if (trainCreate.CheckBoxCountTraining() > 0)
-            {
-            	if(time>30)
-            		trainCreate.Generate_Training_Main(time, true);
-            	else
-            		trainCreate.Generate_Training_Main(time, false);
-            }
-            else
-            {
-            	Alert alert = new Alert(AlertType.INFORMATION);
-            	alert.setTitle("Informacja dla U¯YTKOWNIKA");
-            	alert.setHeaderText(null);
-            	alert.setContentText("Trudno zrobiæ trening bez æwiczeñ. \nProszê wybierz co chcesz dzisiaj trenowaæ.");
-            	alert.showAndWait();
-            }
-        } catch (Exception e)
-        {
-        	Alert alert = new Alert(AlertType.INFORMATION);
-        	alert.setTitle("Informacja dla U¯YTKOWNIKA");
-        	alert.setHeaderText(null);
-        	alert.setContentText("Przepraszamy. Coœ posz³o nie tak. Spróbuj jeszcze raz.");
-        	alert.showAndWait();
-        }
-    }
-    
-    public void Open_Side_Menu()
-    {
-    	
-    }	
+	public void Create_Training() {
+		Training_App.setVisible(true);
+		Settings_App.setVisible(false);
+		Main_App.setVisible(false);
+		Training trainCreate = new Training();
+
+		try {
+			int time = Integer.parseInt(txtbox_hour_CT.getText()) + Integer.parseInt(txtbox_minute_CT.getText());
+			if (trainCreate.CheckBoxCountTraining() > 0) {
+				if (time > 30)
+					trainCreate.Generate_Training_Main(time, true);
+				else
+					trainCreate.Generate_Training_Main(time, false);
+			} else {
+				Alert alert = new Alert(AlertType.INFORMATION);
+				alert.setTitle("Informacja dla U¯YTKOWNIKA");
+				alert.setHeaderText(null);
+				alert.setContentText("Trudno zrobiæ trening bez æwiczeñ. \nProszê wybierz co chcesz dzisiaj trenowaæ.");
+				alert.showAndWait();
+			}
+		} catch (Exception e) {
+			Alert alert = new Alert(AlertType.INFORMATION);
+			alert.setTitle("Informacja dla U¯YTKOWNIKA");
+			alert.setHeaderText(null);
+			alert.setContentText("Przepraszamy. Coœ posz³o nie tak. Spróbuj jeszcze raz.");
+			alert.showAndWait();
+		}
+	}
+
+	public void Open_Side_Menu() {
+
+	}
 }
--- TrainApp/src/application/Training.java
+++ TrainApp/src/application/Training.java
@@ -3,159 +3,132 @@ package application;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Training extends Main
-{
-    List<String> listOfExercises = new LinkedList<>();
-    List<String> exerciseLegs = new LinkedList<>();
-    List<String> exerciseBack = new LinkedList<>();
-    List<String> exerciseChest = new LinkedList<>();
-    List<String> exerciseShoulders = new LinkedList<>();
-    List<String> exerciseArms = new LinkedList<>();
-    List<String> exerciseABS = new LinkedList<>();
+public class Training extends Main {
+	List<String> listOfExercises = new LinkedList<>();
+	List<String> exerciseLegs = new LinkedList<>();
+	List<String> exerciseBack = new LinkedList<>();
+	List<String> exerciseChest = new LinkedList<>();
+	List<String> exerciseShoulders = new LinkedList<>();
+	List<String> exerciseArms = new LinkedList<>();
+	List<String> exerciseABS = new LinkedList<>();
 
+	public void AddExerciseFromDatabase() {
+		listOfExercises.add("LEGS,BACK SQUAT");
+		listOfExercises.add("LEGS,BULGARIAN SQUAT");
+		listOfExercises.add("LEGS,DEADLIFT");
+		listOfExercises.add("BACK,PENDLAY ROW");
+		listOfExercises.add("BACK,DEADLIFT");
+		listOfExercises.add("CHEST,BENCH PRESS");
+		listOfExercises.add("CHEST,BUTTERFLY");
+		listOfExercises.add("SHOULDERS,OVER HEAD PRESS");
+		listOfExercises.add("SHOULDERS,LU RISES");
+		listOfExercises.add("ARMS,PREACHER CURL");
+		listOfExercises.add("ARMS,SKULL CRUSHERS");
+		listOfExercises.add("ABS,PLANK");
+		listOfExercises.add("ABS,CRUNCHES");
+	}
 
-    public void AddExerciseFromDatabase()
-    {
-        listOfExercises.add("LEGS,BACK SQUAT");
-        listOfExercises.add("LEGS,BULGARIAN SQUAT");
-        listOfExercises.add("LEGS,DEADLIFT");
-        listOfExercises.add("BACK,PENDLAY ROW");
-        listOfExercises.add("BACK,DEADLIFT");
-        listOfExercises.add("CHEST,BENCH PRESS");
-        listOfExercises.add("CHEST,BUTTERFLY");
-        listOfExercises.add("SHOULDERS,OVER HEAD PRESS");
-        listOfExercises.add("SHOULDERS,LU RISES");
-        listOfExercises.add("ARMS,PREACHER CURL");
-        listOfExercises.add("ARMS,SKULL CRUSHERS");
-        listOfExercises.add("ABS,PLANK");
-        listOfExercises.add("ABS,CRUNCHES");
-    }
+	public Controller Main_CT_Function() // CT - Create Training
+	{
+		return Main.myConrollerToPass;
+	}
 
-    public Controller Main_CT_Function() // CT - Create Training
-    {
-        return Main.myConrollerToPass;
-    }
+	public void Generate_Training_Main(int time, boolean enoughTime) {
+		Controller myController = Main_CT_Function();
+		ClearExerciseLists();
+		AddExerciseFromDatabase(); // in settings
+		AddExercisesToLists(myController);
+		if (enoughTime)
+			Generate_Training_EnoughTime();
+		else
+			Generate_Training_NoTime();
+	}
 
-    public void Generate_Training_Main(int time, boolean enoughTime)
-    {
-        Controller myController = Main_CT_Function();
-        ClearExerciseLists();
-        AddExerciseFromDatabase(); // in settings
-        AddExercisesToLists(myController);
-if(enoughTime)
-	Generate_Training_EnoughTime();
-	else
-		Generate_Training_NoTime();
-    }
+	public void ClearExerciseLists() {
+		exerciseLegs.clear();
+		exerciseBack.clear();
+		exerciseChest.clear();
+		exerciseShoulders.clear();
+		exerciseArms.clear();
+		exerciseABS.clear();
+	}
 
-    public void ClearExerciseLists()
-    {
-        exerciseLegs.clear();
-        exerciseBack.clear();
-        exerciseChest.clear();
-        exerciseShoulders.clear();
-        exerciseArms.clear();
-        exerciseABS.clear();
-    }
+	public void Generate_Training_EnoughTime() {
 
-    public void Generate_Training_EnoughTime()
-    {
-    	
-    }
-    
-    public void Generate_Training_NoTime()
-    {
-    	
-    }
-    
-    public void AddExercisesToLists(Controller myController)
-    {
-        if (myController.chbox_legs_CT.isSelected())
-        {
-            for (String name : listOfExercises)
-            {
-                String[] words = name.split(",");
-                if (words[0].equals("LEGS"))
-                {
-                    exerciseLegs.add(words[1]);
-                }
-            }
-        }
-        if (myController.chbox_back_CT.isSelected())
-        {
-            for (String name : listOfExercises)
-            {
-                String[] words = name.split(",");
-                if (words[0].equals("BACK"))
-                {
-                    exerciseBack.add(words[1]);
-                }
-            }
-        }
-        if (myController.chbox_chest_CT.isSelected())
-        {
-            for (String name : listOfExercises)
-            {
-                String[] words = name.split(",");
-                if (words[0].equals("CHEST"))
-                {
-                    exerciseChest.add(words[1]);
-                }
-            }
-        }
-        if (myController.chbox_shoulders_CT.isSelected())
-        {
-            for (String name : listOfExercises)
-            {
-                String[] words = name.split(",");
-                if (words[0].equals("SHOULDERS"))
-                {
-                    exerciseShoulders.add(words[1]);
-                }
-            }
-        }
-        if (myController.chbox_arms_CT.isSelected())
-        {
-            for (String name : listOfExercises)
-            {
-                String[] words = name.split(",");
-                if (words[0].equals("ARMS"))
-                {
-                    exerciseArms.add(words[1]);
-                }
-            }
-        }
-        if (myController.chbox_abs_CT.isSelected())
-        {
-            for (String name : listOfExercises)
-            {
-                String[] words = name.split(",");
-                if (words[0].equals("ABS"))
-                {
-                    exerciseABS.add(words[1]);
-                }
-            }
-        }
-    }
+	}
 
-    public int CheckBoxCountTraining()
-    {
-        Controller myController = Main_CT_Function();
-        int count = 0;
-        
-        if (myController.chbox_legs_CT.isSelected())
-            count++;
-        if (myController.chbox_back_CT.isSelected())
-            count++;
-        if (myController.chbox_chest_CT.isSelected())
-            count++;
-        if (myController.chbox_shoulders_CT.isSelected())
-            count++;
-        if (myController.chbox_arms_CT.isSelected())
-            count++;
-        if (myController.chbox_abs_CT.isSelected())
-            count++;
+	public void Generate_Training_NoTime() {
 
-        return count;
-    }
+	}
+
+	public void AddExercisesToLists(Controller myController) {
+		if (myController.chbox_legs_CT.isSelected()) {
+			for (String name : listOfExercises) {
+				String[] words = name.split(",");
+				if (words[0].equals("LEGS")) {
+					exerciseLegs.add(words[1]);
+				}
+			}
+		}
+		if (myController.chbox_back_CT.isSelected()) {
+			for (String name : listOfExercises) {
+				String[] words = name.split(",");
+				if (words[0].equals("BACK")) {
+					exerciseBack.add(words[1]);
+				}
+			}
+		}
+		if (myController.chbox_chest_CT.isSelected()) {
+			for (String name : listOfExercises) {
+				String[] words = name.split(",");
+				if (words[0].equals("CHEST")) {
+					exerciseChest.add(words[1]);
+				}
+			}
+		}
+		if (myController.chbox_shoulders_CT.isSelected()) {
+			for (String name : listOfExercises) {
+				String[] words = name.split(",");
+				if (words[0].equals("SHOULDERS")) {
+					exerciseShoulders.add(words[1]);
+				}
+			}
+		}
+		if (myController.chbox_arms_CT.isSelected()) {
+			for (String name : listOfExercises) {
+				String[] words = name.split(",");
+				if (words[0].equals("ARMS")) {
+					exerciseArms.add(words[1]);
+				}
+			}
+		}
+		if (myController.chbox_abs_CT.isSelected()) {
+			for (String name : listOfExercises) {
+				String[] words = name.split(",");
+				if (words[0].equals("ABS")) {
+					exerciseABS.add(words[1]);
+				}
+			}
+		}
+	}
+
+	public int CheckBoxCountTraining() {
+		Controller myController = Main_CT_Function();
+		int count = 0;
+
+		if (myController.chbox_legs_CT.isSelected())
+			count++;
+		if (myController.chbox_back_CT.isSelected())
+			count++;
+		if (myController.chbox_chest_CT.isSelected())
+			count++;
+		if (myController.chbox_shoulders_CT.isSelected())
+			count++;
+		if (myController.chbox_arms_CT.isSelected())
+			count++;
+		if (myController.chbox_abs_CT.isSelected())
+			count++;
+
+		return count;
+	}
 }
