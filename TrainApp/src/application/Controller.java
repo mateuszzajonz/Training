3cbd6d80b58c65b0b24d3233dda1bd5f54f89231
Maciej Werszler <115154@student.uni.opole.pl>
2021-06-08 15:58:57 +0200

    Corrected accept button and passed data

--- TrainApp/src/application/Controller.java
+++ TrainApp/src/application/Controller.java
@@ -57,6 +57,7 @@ public class Controller {
 	public ImageView img;
 	public Button Side_Menu_btn;
 	public AnchorPane Side_Menu_App;
+	public Label dbConnect;
 
 	// settings app
 	public ComboBox plec, comboCw2, comboCw1;
@@ -102,6 +103,7 @@ public class Controller {
 	public Button reroll_acceptGrid_CT;
 	public TextArea txtArea_acceptGrid_CT;
 	Training trainCreate = new Training();
+	static String trainingToDB;
 
 	String radioGrp, imageId;
 	Image image;
@@ -160,7 +162,9 @@ public class Controller {
 		try {
 			ds = new SQLiteDataSource();
 			ds.setUrl("jdbc:sqlite:sample.dbTraining.db");
+			dbConnect.setText("Connected");
 		} catch (Exception e) {
+			dbConnect.setText("Not Connected");
 			e.printStackTrace();
 			System.exit(0);
 		}
@@ -362,9 +366,9 @@ public class Controller {
 			int count = trainCreate.CheckBoxCountTraining();
 			if (count > 0) {
 				if (time >= 20 * count)
-					trainCreate.Generate_Training_Main(time, true, count);
+					trainingToDB = trainCreate.Generate_Training_Main(time, true, count);
 				else
-					trainCreate.Generate_Training_Main(time, false, count);
+					trainingToDB = trainCreate.Generate_Training_Main(time, false, count);
 			} else {
 				Alert alert = new Alert(AlertType.INFORMATION);
 				alert.setTitle("Informacja dla U¯YTKOWNIKA");
@@ -452,6 +456,7 @@ public class Controller {
 	}
 
 	public void Accept_CT_Action() {
-
+		if (!trainingToDB.equals(""))
+			System.out.println(trainingToDB);
 	}
 }
